9a1f9a364de14b53ef2c9d2f9e6b26811b3edd83
Ivan Balabolkin <balabolkin-x@mail.ru>
2021-06-30 11:01:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-$url = 'http://spaces.ru';
-$ci = curl_init($url);
-curl_setopt($ci, CURLOPT_RETUNRTRANSFER, 1);
-$reponse = curl_exec($ci);
+<?php
+    // ЗДЕСЬ КОД PHP
+    //$url = 'http://spaces.ru';
+//$ci = curl_init($url);
+//curl_setopt($ci, CURLOPT_RETUNRTRANSFER, 1);
+//$reponse = curl_exec($ci);
+?>
+
